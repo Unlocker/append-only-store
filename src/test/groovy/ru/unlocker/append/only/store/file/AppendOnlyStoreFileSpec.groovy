57ae5c627c76ce691321fc9b95ad0cc7540a2225
Maksimov Sergey <sergey.maksimov@mvideo.ru>
2015-08-25 18:19:26 +0300

    Made preparation for using Spock.

---
+++ src/test/groovy/ru/unlocker/append/only/store/file/AppendOnlyStoreFileSpec.groovy
@@ -0,0 +1,12 @@
+package ru.unlocker.append.only.store.file
+
+import spock.lang.*
+import spock.lang.Specification
+
+/**
+ *
+ * @author Sergey Maksimov
+ */
+class AppendOnlyStoreFileSpec extends Specification {
+	
+}
